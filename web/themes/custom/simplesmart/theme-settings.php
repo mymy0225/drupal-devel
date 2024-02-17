24d8d4c6f77f7195b982e8a1972f54b6ee7b67f5
mymy0225 <xametal666@hotmail.co.jp>
2024-02-17 15:17:56 +0900

    bugfix

--- web/themes/custom/simplesmart/theme-settings.php
+++ web/themes/custom/simplesmart/theme-settings.php
@@ -1,15 +1,14 @@
 <?php
 
 function simplesmart_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-  $font = theme_get_setting('font_family');
   $form['font_family'] = array(
       '#type' => 'select',
       '#title' => t('Font Family'),
       '#options' => array(
         '' => 'Default',
-        $font => 'Pacifico',
+        'simplesmart/pacifico' => 'Pacifico',
       ),
       '#description' => t('使用するフォントを選択してください'),
-      '#default_value' => $font,
+      '#default_value' => theme_get_setting('font_family'),
     );
   }
